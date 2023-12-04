2d1a9cf5e93b324cad2a84f336c598e312c44963
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-12-04 10:24:23 -0500

    1.8.2
    added redirect to prevent project page from being hit untill resources to build page are provided.

--- app/page/projects/page.tsx
+++ app/page/projects/page.tsx
@@ -2,15 +2,19 @@
 import ProjectComponent from "@/components/project/projectcomponent";
 import { NextPage } from "next";
 import { Suspense } from "react";
+import { redirect } from "next/navigation"
 
 const Projects: NextPage = async (): Promise<JSX.Element> => {
+
   return (
     <>
       <Suspense>
         <main
           className={`flex min-h-screen flex-col items-center justify-between p-24`}
         >
-          <ProjectComponent />
+       {/*   <ProjectComponent /> */ } 
+       {redirect('/')}
+       
         </main>
       </Suspense>
     </>
